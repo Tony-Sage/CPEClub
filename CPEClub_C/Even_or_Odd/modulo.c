3d6529f1d6a9f7a3c47282651e17b6465b4cb772
Tony-Sage <tonysage339@gmail.com>
2026-04-25 00:27:57 +0100

    Initial code to check if a number is even or odd

---
+++ CPEClub_C/Even_or_Odd/modulo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int num;
+  printf("Enter a number\n");
+  scanf("%d",  &num);
+  
+  if ((num % 2) == 0)
+  {
+    printf("The number is an even number. \n");
+  }
+  else 
+  {
+    printf("The number is an odd number.\n");
+  }
+
+  return 0;
+
+}
