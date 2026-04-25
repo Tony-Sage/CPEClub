04466a718459658fd2a09299ccfce0c2312db4a0
Tony-Sage <tonysage339@gmail.com>
2026-04-25 19:32:04 +0100

    Refactor calculator logic and improve input handling
    Made it so the program doesn't immediately end right after one enters an invalid operand. It now keeps promoting till the user enters a valid operand, then performs the operation

--- CPEClub_C/Calculator/calculator.c
+++ CPEClub_C/Calculator/calculator.c
@@ -2,73 +2,88 @@
 #include <string.h>
 
 void isFloat(int i, float *num, const char s[]);
+int solve(float *num1, float *num2, char operand);
 
 int main()
 {
-    float num1; 
-    float num2;
-    char operand;
+  float num1; 
+  float num2;
+  char operand;
 
-    printf("When prompted, input the two numbers you're working with and the operand\n");
-    printf("Input first number: \n");
-    int result1 = scanf(" %f", &num1);
-    isFloat(result1, &num1,  "first");
+  printf("When prompted, input the two numbers you're working with and the operand\n");
+  printf("Input first number: ");
+  int result1 = scanf(" %f", &num1);
+  isFloat(result1, &num1,  "first");
  
-    printf("Input second number: \n");
-    int result2 = scanf(" %f", &num2);
-    isFloat(result2, &num2, "second");
+  printf("Input second number: ");
+  int result2 = scanf(" %f", &num2);
+  isFloat(result2, &num2, "second");
 
-    printf("Input operand: \n");
-    scanf(" %c", &operand);
+  printf("Input operand: ");
+  scanf(" %c", &operand);
 
-    if (operand == '+')
+  if (operand == '+' || operand == '-' || operand == '/' || operand == '*')
+  {
+    solve(&num1, &num2, operand);
+  }
+
+  else 
+  {
+    while (operand != '+' && operand != '-' && operand != '*' && operand != '/' )
     {
-        printf("Your answer is: %f\n", num1 + num2);
-    }  
-    else if (operand == '-')
-    {
-        printf("Your answer is: %f\n", num1 - num2);
+     printf("Please enter strictly only these characters as operands: +, -, *, and / \n");
+     printf("Enter operand: ");
+     scanf(" %c", &operand);
+     solve(&num1, &num2, operand);
     }
+  }
 
-    else if (operand == '/' || operand == '÷')
-    {
-        if (num2 == 0)
-        {
-            while (num2 == 0)
-            {
-                printf("Division by 0 is not possible. Enter a non zero denominator please!\n");
-                printf("Enter a non zero denominator:  \n");
-                int result3 = scanf("%f", &num2);
-                isFloat(result3, &num2, "second");
-        }
-    }
-        printf("Your answer is: %f\n", num1 / num2);
-    }
-
-    else if (operand == '×' || operand == '*')
-    {
-        printf("Your answer is: %f\n", num1 * num2);
-    }
-
-    else 
-    {
-        printf("Please enter strictly only these characters as operands: +, -, *, /, ÷ and ×");
-    }
-
-    return 0;
-
+  return 0;
 }
 
 void isFloat(int result, float *num, const char s[])
 {
-    if (result != 1)
+  if (result != 1)
+  {
+    while (result != 1)
     {
-        while (result != 1)
-        {
-            while (getchar() != '\n');
-            printf("Enter only digits please!\n");
-            printf("Input %s number: \n", s);
-            result = scanf(" %f", num);
-        }
+      while (getchar() != '\n');
+      printf("Enter only digits please!\n");
+      printf("Input %s number: ", s);
+      result = scanf(" %f", num);
     }
+  }
+}
+
+int solve(float *num1, float *num2, char operand)
+{
+  if (operand == '+')
+  {
+    printf("Your answer is: %f\n", *num1 + *num2);
+  }  
+  else if (operand == '-')
+  {
+    printf("Your answer is: %f\n", *num1 - *num2);
+  }
+
+  else if (operand == '/')
+  {
+    if (*num2 == 0)
+    {
+      while (*num2 == 0)
+      {
+        printf("Division by 0 is not possible. Enter a non zero denominator please!\n");
+        printf("Enter a non zero denominator: ");
+        int result3 = scanf("%f", num2);
+        isFloat(result3, num2, "second");
+      }
+    }
+        
+    printf("Your answer is: %f\n", *num1 / *num2);
+  }
+
+  else
+  {
+    printf("Your answer is: %f\n", *num1 * *num2);
+  }
 }
