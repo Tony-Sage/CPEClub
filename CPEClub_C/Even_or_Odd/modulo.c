ba9a413340eeef3eb53cbb4481f3cc50cfd29c64
Tony-Sage <tonysage339@gmail.com>
2026-04-26 14:35:56 +0100

    Implement input validation for number input
    Added input validation for integer entry in modulo.c.

--- CPEClub_C/Even_or_Odd/modulo.c
+++ CPEClub_C/Even_or_Odd/modulo.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
+void isInteger(int result, int *num);
+
 int main()
 {
 
   int num;
-  printf("Enter a number\n");
-  scanf("%d",  &num);
+  printf("Enter a number: ");
+  int result = scanf("%d",  &num);
+  isInteger(result, &num);
   
   if ((num % 2) == 0)
   {
@@ -18,4 +21,18 @@ int main()
 
   return 0;
 
+}
+
+void isInteger(int result, int *num)
+{
+  if (result != 1)
+  {
+    while (result != 1)
+    {
+      while (getchar() != '\n');
+      printf("Enter only digits please!\n");
+      printf("Enter a number: ");
+      result = scanf(" %d", num);
+    }
+  }
 }
